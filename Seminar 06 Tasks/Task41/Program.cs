0ce966321c02946fd359e944d8c5ad3106534ed0
Pavel Pirogov <televizor50@yandex.ru>
2023-02-16 18:35:14 +0300

    seminar06 tasks done

---
+++ Seminar 06 Tasks/Task41/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] GetArray(string text)
+{
+    Console.Write(text);
+    var numbers = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+    return numbers;
+}
+
+void PrintArray(int[] arr)
+{
+    System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
+}
+
+int CountPositiveNumbers(int[] arr)
+{
+    int result = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            result += 1;
+        }
+    }
+    return result;
+}
+
+int[] arrNumbers = GetArray("Введите несколько чисел через пробел: ");
+PrintArray(arrNumbers);
+System.Console.WriteLine("Количество положительных чисел в массиве = " + CountPositiveNumbers(arrNumbers));
---
+++ Seminar 06 Tasks/Task43/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int GetCoff(string text)
+{
+    Console.Write(text);
+    int coff = Convert.ToInt32(Console.ReadLine());
+    return coff;
+}
+
+void CeckCrossLines(double coffB1, double coffK1, double coffB2, double coffK2)
+{
+    if (coffB1 == coffB2 && coffK1 == coffK2)
+    {
+        System.Console.WriteLine("Прямые совпадают");
+    }
+    else if (coffK1 == coffK2)
+    {
+        System.Console.WriteLine("Прямые параллельны");
+    }
+    else
+    {
+        double x = Math.Round((coffB2 - coffB1) / (coffK1 - coffK2), 1);
+        double y = Math.Round(coffK1 * x + coffB1, 1);
+        System.Console.WriteLine($"Координаты точки пересечения прямых ({x}; {y})");
+    }
+}
+
+int b1 = GetCoff("Прямая №1. Введите коэффициент b1: ");
+int k1 = GetCoff("Прямая №1. Введите коэффициент k1: ");
+int b2 = GetCoff("Прямая №2. Введите коэффициент b2: ");
+int k2 = GetCoff("Прямая №2. Введите коэффициент k2: ");
+
+CeckCrossLines(b1, k1, b2, k2);
