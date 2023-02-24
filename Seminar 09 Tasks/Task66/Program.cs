f9b127aff01b02962eadb8ef2ad221baac65aaf5
Pavel Pirogov <televizor50@yandex.ru>
2023-02-25 00:55:12 +0300

    Seminar09 Tasks Done

---
+++ Seminar 09 Tasks/Task66/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int GetNumber(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetSum(int m, int n)
+{
+    if (m == n) return m;
+    return m + GetSum(m + 1, n);
+}
+
+int M = GetNumber("Введите число от: ");
+int N = GetNumber("Введите число до: ");
+int temp;
+if (M > N)
+{
+    temp = N;
+    N = M;
+    M = temp;
+}
+System.Console.WriteLine(GetSum(M, N));
+
+
---
+++ Seminar 09 Tasks/Task68/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с 
+// помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int GetNumber(string text)
+{
+    System.Console.Write(text);
+    return Math.Abs(Convert.ToInt32(Console.ReadLine()));
+}
+
+int AkkermanFunction(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return AkkermanFunction(m - 1, 1);
+    else return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
+}
+
+int M = GetNumber("Введите первое число: ");
+int N = GetNumber("Введите второе число: ");
+System.Console.WriteLine("A(m,n) = " + AkkermanFunction(M, N));
+
